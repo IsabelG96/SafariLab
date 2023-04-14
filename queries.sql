d219924073bb315777316f418832cea9f72a5160
Isabel <isabelgalwey@gmail.com>
2023-04-14 15:20:49 +0100

    finished MVP

--- queries.sql
+++ queries.sql
@@ -1,6 +1,16 @@
-INSERT INTO animals (name, type, age, enclosure_id)
-VALUES ('Brenda', 'Capybara', 2 , 3);
-INSERT INTO animals (name, type, age, enclosure_id)
-VALUES ('Nala', 'Bengal Tiger', 5 , 1);
+-- INSERT INTO animals (name, type, age, enclosure_id)
+-- VALUES ('Brenda', 'Capybara', 2 , 3);
+-- INSERT INTO animals (name, type, age, enclosure_id)
+-- VALUES ('Nala', 'Bengal Tiger', 5 , 1);
 
-select * from animals;
+-- SELECT animals.name from animals
+-- INNER JOIN enclosures
+-- ON animals.enclosure_id = enclosures.id
+-- WHERE enclosures.id = 1;
+
+SELECT employees.name from employees
+INNER JOIN assignments
+ON employees.id = assignments.employee_id
+INNER JOIN enclosures
+ON assignments.enclosure_id = enclosures.id
+WHERE enclosures.id = 1;
