45cc069567c4415d79d96902138fb65d6a31b5ae
Sarah Abdulle <sarahabdulle1@yahoo.com>
2023-04-14 14:45:53 +0100

    populated animal table

---
+++ queries.sql
@@ -0,0 +1,6 @@
+INSERT INTO animals (name, type, age, enclosure_id)
+VALUES ('Brenda', 'Capybara', 2 , 3);
+INSERT INTO animals (name, type, age, enclosure_id)
+VALUES ('Nala', 'Bengal Tiger', 5 , 1);
+
+select * from animals;
